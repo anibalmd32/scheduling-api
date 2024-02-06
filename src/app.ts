2af3be6c80f207c6b3d39e39258513edb133dd26
Anibal Mendoza <anibalmd32@gmail.com>
2024-02-06 15:15:32 -0400

    modules router

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 
 import { uri, port } from './config/contants'
 import { databaseConnection } from './config/databaseConnection'
+import modulesRouter from './router'
 
 void databaseConnection(uri)
 
@@ -12,6 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
+app.use(modulesRouter)
 
 app.listen(port, () => {
   console.info('Server is running on port ' + port)
