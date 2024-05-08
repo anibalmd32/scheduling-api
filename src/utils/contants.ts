79ae5c54a4e34f28812a942d176e994a7b35e2e7
Anibal Mendoza <anibalmd32@gmail.com>
2024-05-07 23:37:16 -0400

    fix node enviroment

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,4 +1,4 @@
 import 'dotenv/config'
 
-export const uri = process.env.DB_URI
-export const port = process.env.PORT
+export const uri = process.env.NODE_DEV === 'prod' ? process.env.DB_URI : 'mongodb://localhost:27017/scheduling'
+export const port = process.env.NODE_DEV === 'prod' ? process.env.PORT : 8080
