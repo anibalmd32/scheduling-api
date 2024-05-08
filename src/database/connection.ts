c22bdfb7825dd383e30bb58ec60f526a5cc71645
Anibal Mendoza <anibalmd32@gmail.com>
2024-05-08 00:05:08 -0400

    .

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,10 +2,11 @@ import { connect } from 'mongoose'
 
 export async function databaseConnection (uri: string): Promise<void> {
   try {
+    console.info('Connecting to database...', uri)
     await connect(uri)
     console.info('Database has been connected')
   } catch (error) {
-    console.error('Error to connect database')
+    console.error('Error to connect database', error)
     process.exit(1)
   }
 }
