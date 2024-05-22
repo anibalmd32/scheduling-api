ee21e498ff3f6833b1b6fac20f41c921d1f90419
Anibal Mendoza <anibalmd32@gmail>
2024-05-22 13:20:42 -0400

    integrate with fleet editor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,15 +4,15 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {
-    languageOptions: { globals: globals.browser },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      "@typescript-eslint/no-var-requires": 0,
-      "semi": 2
-    },
-  }
+	{
+		languageOptions: {globals: globals.browser},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		rules: {
+			"@typescript-eslint/no-var-requires": 0,
+			"semi": 2
+		},
+	}
 ];
--- src/modules/professors/services.ts
+++ src/modules/professors/services.ts
@@ -1,110 +1,109 @@
-import Professors from './model'
-import Schedule from '../schedules/model'
-import { type ProfessorsDTO, type ProfessorSchema, type ProfessorsData } from './definitions'
-import { formatEvent } from '../../libs/formatEvent';
-import { ScheduleEvent } from '../schedules/definitions';
+import Professors from './model';
+import Schedule from '../schedules/model';
+import {type ProfessorsDTO, type ProfessorSchema, type ProfessorsData} from './definitions';
+import {formatEvent} from '../../libs/formatEvent';
 
 export class ProfessorServices {
-  async getProfessors(): Promise<ProfessorsData[]> {
-    const professors = await Professors.find({});
+	async getProfessors(): Promise<ProfessorsData[]> {
+		const professors = await Professors.find({});
 
-    const data = professors.map(professor => {
-      if (professor.schedule.length === 0) {
-        return {
-          _id: String(professor._id),
-          condition: professor.condition,
-          data: professor.data,
-          schedule: []
-        }
-      } else {
-        const schedules = professor.schedule.map(schedule => {
-          const scheduleEvent = formatEvent({
-            _id: String(schedule._id),
-            classroom: schedule.classroom,
-            day: schedule.day,
-            endTime: schedule.endTime,
-            startTime: schedule.startTime,
-            subject: schedule.subject,
-            degree: schedule.degree,
-            semester: schedule.semester,
-            extra: {
-              hourInterval: schedule.extra.hourInterval,
-              subjectType: schedule.extra.subjectType
-            }
-          }, 'professor')
+		const data = professors.map(professor => {
+			if (professor.schedule.length === 0) {
+				return {
+					_id: String(professor._id),
+					condition: professor.condition,
+					data: professor.data,
+					schedule: []
+				};
+			} else {
+				const schedules = professor.schedule.map(schedule => {
+					const scheduleEvent = formatEvent({
+						_id: String(schedule._id),
+						classroom: schedule.classroom,
+						day: schedule.day,
+						endTime: schedule.endTime,
+						startTime: schedule.startTime,
+						subject: schedule.subject,
+						degree: schedule.degree,
+						semester: schedule.semester,
+						extra: {
+							hourInterval: schedule.extra.hourInterval,
+							subjectType: schedule.extra.subjectType
+						}
+					}, 'professor');
 
-          return scheduleEvent
-        })
+					return scheduleEvent;
+				});
 
-        return {
-          _id: String(professor._id),
-          condition: professor.condition,
-          data: professor.data,
-          schedule: schedules
-        }
-      }
-    })
+				return {
+					_id: String(professor._id),
+					condition: professor.condition,
+					data: professor.data,
+					schedule: schedules
+				};
+			}
+		});
 
-    return data
-  }
+		return data;
+	}
 
-  async addProfessor (data: ProfessorsDTO): Promise<void> {
+	async addProfessor(data: ProfessorsDTO): Promise<void> {
 
-    const dataForAdd: ProfessorSchema = {
-      condition: data.condition,
-      data: data.data,
-      schedule: []
-    }
+		const dataForAdd: ProfessorSchema = {
+			condition: data.condition,
+			data: data.data,
+			schedule: []
+		};
 
-    const newProfessor = await Professors.create(dataForAdd)
+		const newProfessor = await Professors.create(dataForAdd);
 
-    await newProfessor.save()
-  }
+		await newProfessor.save();
+	}
 
-  async asingSchedule (shecheduleId: string, professorId: string): Promise<void> {
-    const scheduleForAsign = await Schedule.findById(shecheduleId)
-    
-    await Professors.findByIdAndUpdate(
-      professorId,
-      {
-        $push: {
-          schedule: scheduleForAsign
-        }
-      }
-    )
-  }
+	async asingSchedule(shecheduleId: string, professorId: string): Promise<void> {
+		const scheduleForAsign = await Schedule.findById(shecheduleId);
 
-  async removeSchedule (scheduleId: string, professorId: string): Promise<void> {
-  
-    await Professors.updateOne(
-        { _id: professorId },
-        {
-            $pull: {
-                schedule: { _id: scheduleId }
-            }
-        }
-    );
-  }
+		await Professors.findByIdAndUpdate(
+			professorId,
+			{
+				$push: {
+					schedule: scheduleForAsign
+				}
+			}
+		);
+	}
 
-  async updateProfessor(professorId: string, professorData: ProfessorsDTO): Promise<ProfessorsData> {
-    
-    const updatedProfessor = await Professors.findByIdAndUpdate(
-      professorId,
-      { data: professorData },
-      { new: true }
-    )
+	async removeSchedule(scheduleId: string, professorId: string): Promise<void> {
 
-    if (!updatedProfessor) throw new Error('No se pudo actualizar la informacion del profesor')
+		await Professors.updateOne(
+			{_id: professorId},
+			{
+				$pull: {
+					schedule: {_id: scheduleId}
+				}
+			}
+		);
+	}
 
-    return {
-      _id: String(updatedProfessor?._id),
-      condition: updatedProfessor.condition,
-      data: updatedProfessor.data,
-      schedule: updatedProfessor.schedule
-    }
-  }
+	async updateProfessor(professorId: string, professorData: ProfessorsDTO): Promise<ProfessorsData> {
 
-  async deleteProfessor(professorId: string): Promise<void> {
-    await Professors.findByIdAndDelete(professorId)
-  }
+		const updatedProfessor = await Professors.findByIdAndUpdate(
+			professorId,
+			{data: professorData},
+			{new: true}
+		);
+
+		if (!updatedProfessor) throw new Error('No se pudo actualizar la informacion del profesor');
+
+		return {
+			_id: String(updatedProfessor?._id),
+			condition: updatedProfessor.condition,
+			data: updatedProfessor.data,
+			schedule: updatedProfessor.schedule
+		};
+	}
+
+	async deleteProfessor(professorId: string): Promise<void> {
+		await Professors.findByIdAndDelete(professorId);
+	}
 }
