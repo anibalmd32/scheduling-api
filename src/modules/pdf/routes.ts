af9b2e19156be2295cdd46546fa84686dc515d8b
Anibal Mendoza <anibalmd32@gmail>
2024-05-27 01:32:47 -0400

    args for puppeteer

--- src/modules/pdf/routes.ts
+++ src/modules/pdf/routes.ts
@@ -9,9 +9,21 @@ router.get('/', async (req, res) => {
 
   try {
 		const browser = await puppeteer.launch({
-			headless: true,
-			args: ['--no-sandbox']
+			headless: 'shell',
+			args: [
+				"--disable-gpu",
+				"--disable-dev-shm-usage",
+				"--disable-setuid-sandbox",
+				"--no-first-run",
+				"--no-sandbox",
+				"--no-zygote",
+				"--single-process",
+			],
+			ignoreDefaultArgs: [
+				'--disable-extensions',
+			]
 		})
+		
 		const page = await browser.newPage()
 		await page.goto(`http://100.27.219.153:3000/print?for=${forType}&id=${id}`)
 		const pdf = await page.pdf({ format: 'A4' })
